ca11443194bac7d4c85466887bfb0881ebd60895
Rimsha Maredia <39537734+Rimshamaredia@users.noreply.github.com>
2019-02-10 18:55:32 -0600

    Update shell-sort.cpp

--- shell-sort.cpp
+++ shell-sort.cpp
@@ -12,7 +12,14 @@
 void
 ShellSort::sort(int A[], int size)
 {
-  /* Complete this function with the implementation of shell sort algorithm 
-  Record number of comparisons in variable num_cmps of class Sort
-  */
+  for(int gap = size/2;gap>0;gap/=2){
+  for(int i = gap;i<size;i++){
+    int temp = sort[i];
+    int j ;
+    for(j = i;j>=gap && sort[j-gap]>temp;j-=gap){
+      sort[j]=sort[j-gap];
+    }
+    sort[j]=temp;
+  }
+}
 }
